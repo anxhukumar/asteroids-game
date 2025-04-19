cd1b107878aacbba1f5f18fc72c481adacc985ea
anxhukumar <contact.anshukumar@protonmail.com>
2025-04-19 17:30:33 +0530

    feat: add rotation with a and d key

--- main.py
+++ main.py
@@ -5,25 +5,30 @@ from player import Player
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  dt = 0
   clock = pygame.time.Clock()
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT / 2
+  player = Player(x, y)
+  dt = 0
 
   while(True):
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return 
     
-    screen.fill("black")
-    
     # limit FPS to 60
     delta = clock.tick(60)
     dt = delta/1000
 
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-    player.draw(screen)
-    pygame.display.flip()
+    player.update(dt)
 
+    screen.fill("black")
+    
+    # adding player to screen
+    player.draw(screen)
+    
+    # update the screen
+    pygame.display.flip()
+    
 if __name__ == "__main__":
   main()
