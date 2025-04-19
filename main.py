1071736eaf3a62823f424aa974e049aab2b51b0c
anxhukumar <contact.anshukumar@protonmail.com>
2025-04-19 15:27:57 +0530

    feat: limit fps to 60

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from constants import *
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  dt = 0
+  clock = pygame.time.Clock()
 
   while(True):
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return 
-    screen.fill("green")
+    
+    screen.fill("black")
     pygame.display.flip()
 
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
+    # limit FPS to 60
+    delta = clock.tick(60)
+    dt = delta/1000
 
 if __name__ == "__main__":
   main()
